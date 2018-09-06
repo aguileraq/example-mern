387be66d6c40ab66cca628d66e4d50e2b026045c
carlos <aguileraq@gmail.com>
2018-09-06 05:35:12 -0500

    Router

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
-export default () => {
-  return (
-    <nav className="light-blue lighten-1" role="navigation">
-    <div className="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
-      <ul className="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-
-      <ul id="nav-mobile" className="sidenav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-    </div>
-  </nav>
-  )
+class Navbar extends Component {
+  render(){
+    return (
+      <nav className="light-blue lighten-1" role="navigation">
+        <div className="nav-wrapper container"><Link id="logo-container" to="/" class="brand-logo">Logo</Link>
+          <ul className="right hide-on-med-and-down">
+            <li><Link to="/register">Registrar</Link></li>
+          </ul>
+  
+          <ul id="nav-mobile" className="sidenav">
+            <li><a href="#">Navbar Link</a></li>
+          </ul>
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        </div>
+      </nav>
+    )
+  }
 }
+export default Navbar;
